675d5981c10627b71c980074cb70bebf600bdc09
arque1393 <79799118+arque1393@users.noreply.github.com>
2021-03-01 18:46:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var slider = document.getElementById('input2')
+var output = document.getElementById('rangevalue')
+function display(){    
+    output.innerText = slider.value +'%'
 }
-        
+
+var list1 = document.getElementById('list')
+for (var i = 1; i<=100; i++){
+    var o1 = document.createElement('option')
+    o1.value = i;
+    o1.innerHTML = i;
+    list1.options.add(o1)
+}
+
+function si(){
+    a = Number(document.getElementById('input1').value);
+    b = Number(document.getElementById('input2').value);
+    c = Number(document.getElementById('list').value);
+    d = (a*b*c)/100;
+    x = new Date();
+    x = 2000 + x.getYear()%100 + c;
+    p = document.getElementById('1');
+    p.innerHTML = 'If you deposit ' + a.toString() + '<br>at an interest rate of '+b.toString()+'% , <br>you will recive an amount ' + d.toString()+' ,<br> in the year ' + x.toString()  ;
+}
